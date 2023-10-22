79256193dab8b5e0204f472580380f161f5c5f57
ritik <ritikbhatt020@gmail.com>
2023-10-22 23:22:53 +0530

    Added Diabetes, heart and Parkinson activity and designed choose activity

--- app/src/main/java/com/example/multiplediseasepredictionapp/ChooseDiseaseActivity.kt
+++ app/src/main/java/com/example/multiplediseasepredictionapp/ChooseDiseaseActivity.kt
@@ -1,11 +1,32 @@
 package com.example.multiplediseasepredictionapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ChooseDiseaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose_disease)
+
+        val diabetesBtn = findViewById<Button>(R.id.diabetesBtn)
+        val heartBtn = findViewById<Button>(R.id.heartBtn)
+        val parkBtn = findViewById<Button>(R.id.parkinsonBtn)
+
+        diabetesBtn.setOnClickListener {
+            val intent = Intent(this, DiabetesActivity::class.java)
+            startActivity(intent)
+        }
+
+        heartBtn.setOnClickListener {
+            val intent = Intent(this, HeartActivity::class.java)
+            startActivity(intent)
+        }
+
+        parkBtn.setOnClickListener {
+            val intent = Intent(this, ParkinsonActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/multiplediseasepredictionapp/HeartActivity.kt
@@ -0,0 +1,11 @@
+package com.example.multiplediseasepredictionapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HeartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_heart)
+    }
+}
