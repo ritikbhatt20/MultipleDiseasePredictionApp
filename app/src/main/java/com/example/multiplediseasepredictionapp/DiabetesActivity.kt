00c0a4fa9aa187a682c51865c7d8dd17100a3c74
ritik <ritikbhatt020@gmail.com>
2023-12-12 17:08:02 +0530

    Made the Heart Activity UI

--- app/src/main/java/com/example/multiplediseasepredictionapp/DiabetesActivity.kt
+++ app/src/main/java/com/example/multiplediseasepredictionapp/DiabetesActivity.kt
@@ -2,10 +2,22 @@ package com.example.multiplediseasepredictionapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class DiabetesActivity : AppCompatActivity() {
+
+    var url = "https://disease-prediction-g81g.onrender.com/predictD"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_diabetes)
+
+        val cgpa = findViewById<EditText>(R.id.cgpa)
+        val iq = findViewById<EditText>(R.id.iq)
+        val profile_score = findViewById<EditText>(R.id.profileScore)
+        val predict = findViewById<Button>(R.id.btn)
+        val result = findViewById<TextView>(R.id.result)
+
     }
 }
